85e62f28950788ced84a5340edd09f303d8ea7d5
Ryan Miller <ryan.miller.p@gmail.com>
2025-06-07 20:20:28 -0400

    Test auto-formatting functionality

---
+++ test-auto-format.js
@@ -0,0 +1,9 @@
+// Test file with MORE formatting issues
+const reallyBadFormatting = { x: 1, y: 2, z: 3, a: 4 };
+if (reallyBadFormatting) {
+	console.log('this is terrible formatting');
+}
+const anotherBadExample = { foo: 'bar', baz: 'qux' };
+function badFunction() {
+	return 'not formatted';
+}
